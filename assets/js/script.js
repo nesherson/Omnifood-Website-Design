8c8ba1cf4ecb9ceb0fccb022231b6285179a7869
Nešad Omeragić <nesad.omeragich27@gmail.com>
2020-01-08 17:42:25 +0100

    Finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ $(document).ready(function() {
       offset: "60px;"
     }
   );
+
+  /* MOBILE NAVIGATION */
+  $(".js--mobile-nav-icon").click(function() {
+    var nav = $(".js--main-nav");
+    nav.slideToggle(200);
+  });
   /* Scroll on buttons */
   $(".js--scroll-to-plans").click(function() {
     $("html, body").animate(
@@ -27,48 +33,37 @@ $(document).ready(function() {
     );
   });
 
-  /* Navigation scroll*/
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            1000,
-            function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
+  /*Animations on scroll*/
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "70%"
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "70%"
+    }
+  );
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "60%"
+    }
+  );
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%"
+    }
+  );
 });
